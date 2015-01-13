03f487d887b60c59fa52c982bd20b8b6e39b68ae
MeepoKid <area51alienware@gmail.com>
2015-01-12 16:28:04 -0800

    CameraDefine.cpp
    All of the camera defines.

---
+++ CameraDefine.cpp
@@ -0,0 +1,16 @@
+//Camrea constants used for distance calculation 
+#define Y_IMAGE_RES 240 //320x240 res
+#define VIEW_ANGLE 49
+
+#define PI 3.131592653
+//Score limits used for target identification
+#define RECTANGULARITY_LIMIT 40
+#define ASPECT_RATIO_LIMIT 55
+//score limits used for hot target determination
+#define TAPE_WIDTH_LIMIT 50
+#define VERTICAL_SCORE_LIMIT 50
+#define LR_SCORE_LIMIT 50
+//minimum area of particles to be considered
+#define AREA_MINIMUM 150
+//maximum number of particcles to process
+#define MAX_PARTICILES 8
