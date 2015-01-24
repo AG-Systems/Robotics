c94917482552feeef11256011ee8d179afd7369e
MeepoKid <area51alienware@gmail.com>
2015-01-23 16:49:26 -0800

    DriveAuto.hpp

---
+++ DriveAuto.hpp
@@ -0,0 +1,20 @@
+#ifndef DRIVE_AUTO_HPP
+#define DRIVE_AUTO_HPP
+
+#include <TwoMotorGroup.hpp>
+#include <iostream>
+#include <WPILib.h>
+class DriveAuto {
+public:
+	void move(float feet, float velocity);
+	void axisTurn(float degrees);
+
+private:
+	const std::shared_ptr<Encoder> leftEncoder;
+	const std::shared_ptr<Encoder> rightEncoder;
+	const std::shared_ptr<Gyro> gyro;
+	const std::unique_ptr<TwoMotorGroup> leftMotors;
+	const std::unique_ptr<TwoMotorGroup> rightMotors;
+};
+
+#endif
