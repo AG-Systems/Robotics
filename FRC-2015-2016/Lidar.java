db06b75ac32415297ab3cd44f0a098809cb884b5
Auriga <AG-Systems@users.noreply.github.com>
2016-01-15 10:08:29 -0800

    Update Lidar.java

--- FRC-2015-2016/Lidar.java
+++ FRC-2015-2016/Lidar.java
@@ -1,1 +1,15 @@
+/*
+Insert all of the robo libs
 
+*/
+
+	private final int LIDAR_ADDR = 0x62;
+	private final int LIDAR_CONFIG_REGISTER = 0x00;
+	private final int LIDAR_DISTANCE_REGISTER = 0x8f;
+	
+	public void start()
+	{
+	  // start sudo code
+	}
+	
+	
